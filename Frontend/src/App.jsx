9a0f2da9c4c85a3b7fee9e8762c5da639577f632
jeenasaurabh22-tech <jeenasaurabh22@gmail.com>
2026-06-29 22:43:15 +0530

    improved context

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <AuthContextProvider>
       <RouterProvider router={router} />
     </AuthContextProvider>
+    //this is the main entry point of the application, where we wrap our app with the AuthContextProvider to provide authentication context to all components, and use RouterProvider to handle routing based on the defined routes in app.routes.jsx.
   )
 }
 
